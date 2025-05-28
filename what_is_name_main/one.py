225ca00012e8b6707d87efbf9c2ebb809b6dd34d
Phantom-VK <vikramadityakhupse@gmail.com>
2025-05-28 20:37:24 +0530

    How __name__=="__main__: works.

---
+++ what_is_name_main/one.py
@@ -0,0 +1,3 @@
+import two
+# Try running  this file with and without import and then run two.py solely
+print("Inside file two")
---
+++ what_is_name_main/two.py
@@ -0,0 +1,4 @@
+print("I am here")
+
+if __name__ == "__main__":
+    print("Inside file one")
