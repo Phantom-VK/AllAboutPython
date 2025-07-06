43e2a756f866af06a5b5483fb9b6172b1339e696
Phantom-VK <vikramadityakhupse@gmail.com>
2025-07-06 21:26:52 +0530

    Removed

--- swapcase.py
+++
@@ -1,9 +0,0 @@
-#https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
-def swap_case(s):
-    swapped_string = ""
-    for char in s:
-        if char.isupper():
-            swapped_string += char.lower()
-        else:
-            swapped_string += char.upper()
-    return swapped_string
